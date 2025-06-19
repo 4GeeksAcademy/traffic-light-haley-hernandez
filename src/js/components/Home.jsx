5f90bb6cef44c12834eda596e9476621a33a3a21
Haley <haleymariehernandez10@gmail.com>
2025-06-19 00:45:45 +0000

    the final final

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,9 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 //create your first component
 const Home = () => {
 	const [activeLight, setActiveLight] = useState(null);
 	const toggleLightClick = (color) => { setActiveLight(color); }
+
+	// addings notes here to make sure i understand what my code is doing..
+	useEffect(() => {
+    // set up an interval to change lights automatically
+    const interval = setInterval(() => {
+      // ligh order it will change: red → green → yellow → red
+      if (activeLight === "red") {
+        setActiveLight("green");
+      } else if (activeLight === "green") {
+        setActiveLight("yellow");
+      } else if (activeLight === "yellow") {
+        setActiveLight("red");
+      }
+    }, 3000); // this means change every 3 seconds (3000 milliseconds)
+
+    
+    return () => clearInterval(interval);
+  }, [activeLight]); //re-run if clicked
 
 	return (
 		<div className="app">
